717ab80d6be989f7db9367ca82d30484ed59f2e7
AndrewHripun <132257740+AndrewHripun@users.noreply.github.com>
2023-05-01 11:30:51 +0300

    Add files via upload
    01/05/2023

---
+++ backLinksChecker/src/main/java/backLinksChecker/LinksTypes.java
@@ -0,0 +1,6 @@
+package backLinksChecker;
+
+public class LinksTypes {
+	int length = 0; // это длина создаваемого массива
+	String[] linksContainArray = new String[length]; // массив из length элементов типа String
+}
