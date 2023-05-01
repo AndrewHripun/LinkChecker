cb066cb4df683d06c7d13a7e641d7e2c4d9b4642
AndrewHripun <132257740+AndrewHripun@users.noreply.github.com>
2023-05-01 11:53:28 +0300

    Delete backLinksChecker directory

--- backLinksChecker/src/main/java/backLinksChecker/LinksTypes.java
+++
@@ -1,6 +0,0 @@
-package backLinksChecker;
-
-public class LinksTypes {
-	int length = 0; // это длина создаваемого массива
-	String[] linksContainArray = new String[length]; // массив из length элементов типа String
-}
