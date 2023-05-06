557bc81f3e59f0c1bf819a8e161e8c275efd6bb0
AndrewHripun <132257740+AndrewHripun@users.noreply.github.com>
2023-05-06 14:09:29 +0300

    Add files via upload

--- src/main/java/backLinksChecker/SeleniumLinkChecker.java
+++ src/main/java/backLinksChecker/SeleniumLinkChecker.java
@@ -3,7 +3,13 @@ package backLinksChecker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import java.io.File;
+
+
+
 
 import java.util.List;
 
@@ -11,8 +17,12 @@ public class SeleniumLinkChecker {
 	private WebDriver driver;
 
 	 public SeleniumLinkChecker() { //конструктор класса инициализирующий объект
-	        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application");
-	        this.driver = new ChromeDriver(); //сохраняем его в переменной
+	        System.setProperty("webdriver.gecko.driver", "F:\\FireFox\\geckodriver.exe");
+	        FirefoxBinary firefoxBinary = new FirefoxBinary();
+	        firefoxBinary.setExecutable(new File("F:\\FireFox\\firefox.exe")); //указываем путь к Firefox браузеру
+	        FirefoxOptions firefoxOptions = new FirefoxOptions();
+	        firefoxOptions.setBinary(firefoxBinary);
+	        this.driver = new FirefoxDriver(); //сохраняем его в переменной
 	    }
 
 	    public void checkLink(String link, String url) { //метод которым проверяем ссылки переходя по урлам
